342ca653ec73f827dc60dfb920bcb8e860b1fe99
Richard de Borja <richard.deborja@gmail.com>
2015-04-14 20:14:29 -0400

    updated stepplot theme for x and y tick marks

--- R/default.stepplot.theme.R
+++ R/default.stepplot.theme.R
@@ -1,4 +1,16 @@
-default.stepplot.theme <- function(base_size = 30, base_family = 'Helvetica') {
+default.stepplot.theme <- function(base_size=30, base_family='Helvetica', xticks=TRUE, yticks=TRUE) {
+  if (xticks==FALSE) {
+    theme_bw(base_size = base_size, base_family = base_family) %+replace%
+      theme(
+        axis.text.x = element_text(size = rel(0.8), hjust = 1)
+      )
+    }
+  if (yticks==FALSE) {
+    theme_bw(base_size = base_size, base_family = base_family) %+replace%
+      theme(
+        axis.text.x = element_text(size = rel(0.8), hjust = 1)
+      )    
+    }
   theme_bw(base_size = base_size, base_family = base_family) %+replace%
     theme(
       axis.text.x = element_text(size = rel(0.8), hjust = 1)
